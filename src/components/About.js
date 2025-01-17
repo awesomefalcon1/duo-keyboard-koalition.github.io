030d1ef080e76c89239363cc6ff8ed6ef1163341
Darcy Liu <darcy.ldx@gmail.com>
2025-01-17 18:12:33 -0500

    Removed pirate

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About() {
           The <strong>Duo Keyboard Koalition</strong> is a community of passionate hackers, coders, and tech enthusiasts who come together to collaborate, learn, and take on hackathons with a competitive spirit. Originally formed by a group of people who met at hackathons, the Koalition has evolved into a space where members push each other to innovate, build meaningful projects, and grow their skills.
         </p>
         <p>
-          The vibe is part competitive, part collaborative—like a team of modern-day "pirates" setting out on adventures in tech, always ready to tackle the next challenge. Whether you're looking to brainstorm new ideas, work on side projects, or prepare for upcoming hackathons, the Duo Keyboard Koalition is a supportive and driven community where you can connect with like-minded people and bring exciting ideas to life.
+          The vibe is part competitive, part collaborative—like a team of modern-day clan setting out on adventures in tech, always ready to tackle the next challenge. Whether you're looking to brainstorm new ideas, work on side projects, or prepare for upcoming hackathons, the Duo Keyboard Koalition is a supportive and driven community where you can connect with like-minded people and bring exciting ideas to life.
         </p>
       </div>
     </section>
