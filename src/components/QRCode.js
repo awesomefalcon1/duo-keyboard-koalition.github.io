5584950da22b984f3bc184eb979862d5749d64dd
Darcy Liu <darcy.ldx@gmail.com>
2025-01-18 13:17:14 -0500

    text-white

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -4,7 +4,7 @@ import { QRCodeSVG } from 'qrcode.react';
 function QRCode() {
   return (
     <section className="max-w-3xl mx-auto text-center">
-      <h2 className="text-3xl font-bold mb-6 mt-6">Scan Our QR Code</h2>
+      <h2 className="text-3xl font-bold mb-6 mt-6 text-white">Scan Our QR Code</h2>
       <div className="bg-white p-8 rounded-lg inline-block">
         <QRCodeSVG 
           value="https://discord.gg/6GaWZAawUc"
