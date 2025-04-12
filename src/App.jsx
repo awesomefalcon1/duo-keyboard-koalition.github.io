c8f4ee347c03b22bea3797a49ac240766be70614
darcy <darcy.ldx@gmail.com>
2025-04-11 23:01:04 -0400

    goodby gh pages

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './components/Home';
+//import Footer from './components/Footer';
+
+
+function App() {
+
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col bg-black text-white">
+        <main className="flex-grow container mx-auto px-4 py-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          
+        </main>
+        {/* 
+        <Footer handleDiscordJoin={handleDiscordJoin} />
+      Footer */}
+        </div>
+    </Router>
+  );
+}
+
+export default App;
